d1e3fb9406bf44f989fc34b2c3238b12194d48a2
René Oelke <rene.oelke@gmail.com>
2013-11-20 14:39:20 +0100

    Changed dependencies and version.

--- metadata.rb
+++ metadata.rb
@@ -3,14 +3,14 @@ maintainer_email  "rene.oelke@foobugs.com"
 license           "Apache 2.0"
 description       "Installs and configures PHP debugger."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0"
+version           "1.0.1"
 
 recipe            "default", "Common configuration."
 recipe            "xdebug", "Installs and configures XDebug."
 recipe            "zend", "Installs and configures Zend Debugger."
 
-depends           "php"
-
 %w{ ubuntu }.each do |os|
   supports os
 end
+
+depends "build-essential"
